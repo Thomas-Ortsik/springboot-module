c9c2014a4e3c2f2e6b585aae03dc6eb37a0e08cc
Thomas Ortsik <doctor.strangethumb@gmail.com>
2020-03-23 19:57:56 +0100

    i hate Spring

---
+++ exercises/week3/exercise4/src/main/java/academy/everyonecodes/advtypes/endpoints/CommunityEventEndpoint.java
@@ -0,0 +1,23 @@
+package academy.everyonecodes.advtypes.endpoints;
+
+import jdk.jfr.Event;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/events")
+public class CommunityEventEndpoint {
+    private final List<Event> events;
+
+    public CommunityEventEndpoint(List<Event> events) {
+        this.events = events;
+    }
+
+    @GetMapping
+    List<Event> events(){
+        return events;
+    }
+}
---
+++ exercises/week3/exercise4/src/test/java/academy/everyonecodes/advtypes/AdvancedTypesApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.everyonecodes.advtypes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedTypesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
